d94049af024e29ee75f62861dcc41289b7924161
bigbrotherisstillwatching <140657673+bigbrotherisstillwatching@users.noreply.github.com>
2024-03-20 11:56:20 +0100

    Update equalizer_stop.sh

--- scripts/equalizer_stop.sh
+++ scripts/equalizer_stop.sh
@@ -4,8 +4,8 @@ sleep 0.5
 
 if [[ $(pacmd list-sinks | grep -A1 '* index:' | sed 's/.*<//; s/>.*//' | grep -v '* index:') == equalizer ]]
 then
-   pactl set-default-sink $(head -1 /home/phablet/.config/radio.s710/radio.s710.defaultsink.txt)
-   pactl unload-module $(head -1 /home/phablet/.config/radio.s710/radio.s710.oldsink.txt)
+   pactl set-default-sink $(head -1 /home/phablet/.config/utequalizer.bigbrotherisstillwatching/utequalizer.bigbrotherisstillwatching.defaultsink.txt)
+   pactl unload-module $(head -1 /home/phablet/.config/utequalizer.bigbrotherisstillwatching/utequalizer.bigbrotherisstillwatching.oldsink.txt)
 else
    :
 fi
