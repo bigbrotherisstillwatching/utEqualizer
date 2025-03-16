7a0df5aac4954681b66b3cb22e8869d96eaccd57
bigbrotherisstillwatching <140657673+bigbrotherisstillwatching@users.noreply.github.com>
2025-03-16 18:38:41 +0100

    Update equalizer_stop.sh

--- scripts/equalizer_stop.sh
+++ scripts/equalizer_stop.sh
@@ -4,8 +4,8 @@ sleep 0.5
 
 if [[ $(pacmd list-sinks | grep -A1 '* index:' | sed 's/.*<//; s/>.*//' | grep -v '* index:') == equalizer ]]
 then
-   pactl set-default-sink $(head -1 /home/phablet/.config/utequalizer.bigbrotherisstillwatching/utequalizer.bigbrotherisstillwatching.defaultsink.txt)
-   pactl unload-module $(head -1 /home/phablet/.config/utequalizer.bigbrotherisstillwatching/utequalizer.bigbrotherisstillwatching.oldsink.txt)
+   pacmd set-default-sink $(head -1 /home/phablet/.config/utequalizer.bigbrotherisstillwatching/utequalizer.bigbrotherisstillwatching.defaultsink.txt)
+   pacmd unload-module $(head -1 /home/phablet/.config/utequalizer.bigbrotherisstillwatching/utequalizer.bigbrotherisstillwatching.oldsink.txt)
 else
    :
 fi
