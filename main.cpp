02f62b24fe50b87404b7b13f80b46dab76824b93
bigbrotherisstillwatching <140657673+bigbrotherisstillwatching@users.noreply.github.com>
2025-10-22 15:48:08 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <QGuiApplication>
+//#include <QGuiApplication>
 #include <QCoreApplication>
 #include <QUrl>
 #include <QString>
