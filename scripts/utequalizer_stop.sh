2f8ca853b4cff4e9b856e6d113fd096539502b4d
bigbrotherisstillwatching <140657673+bigbrotherisstillwatching@users.noreply.github.com>
2026-01-21 21:12:23 +0100

    Update utequalizer_stop.sh

--- scripts/utequalizer_stop.sh
+++ scripts/utequalizer_stop.sh
@@ -5,7 +5,7 @@ sleep 0.5
 #if [[ $(pacmd list-sinks | grep -A1 '* index:' | sed 's/.*<//; s/>.*//' | grep -v '* index:') == equalizer ]]
 #then
 kill $(pgrep -f utequalizer_start.sh)
-kill $(pgrep -f utequalizer_change.sh)
+kill $(pgrep -f move_sink_inputs_to_utequalizer.sh)
 #pacmd set-default-sink $(head -1 /home/phablet/.config/utequalizer.bigbrotherisstillwatching/utequalizer.bigbrotherisstillwatching.defaultsink.txt)
 pactl list short sink-inputs | awk '{print $1}' | xargs -I {} pacmd move-sink-input {} $(head -1 /home/phablet/.config/utequalizer.bigbrotherisstillwatching/utequalizer.bigbrotherisstillwatching.defaultsink.txt)
 pacmd unload-module $(head -1 /home/phablet/.config/utequalizer.bigbrotherisstillwatching/utequalizer.bigbrotherisstillwatching.utequalizermodule.txt)
