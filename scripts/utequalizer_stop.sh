64545e40379416fe61a3f632d8a11084161d538d
bigbrotherisstillwatching <140657673+bigbrotherisstillwatching@users.noreply.github.com>
2026-01-21 22:26:03 +0100

    Update utequalizer_stop.sh

--- scripts/utequalizer_stop.sh
+++ scripts/utequalizer_stop.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-var1=$(pgrep -f utequalizer_start.sh)
-var2=$(pgrep -f move_sink_inputs_to_utequalizer.sh)
+#var1=$(pgrep -f utequalizer_start.sh)
+#var2=$(pgrep -f move_sink_inputs_to_utequalizer.sh)
 
 sleep 0.5
 
@@ -9,12 +9,14 @@ sleep 0.5
 #then
 #kill $(pgrep -f utequalizer_start.sh)
 #kill $(pgrep -f move_sink_inputs_to_utequalizer.sh)
-if ! [ "x$var1" = "x"  ]; then
-    kill "$var1"
-fi
-if ! [ "x$var2" = "x"  ]; then
-    kill "$var2"
-fi
+#if ! [ "x$var1" = "x"  ]; then
+#    kill "$var1"
+#fi
+#if ! [ "x$var2" = "x"  ]; then
+#    kill "$var2"
+#fi
+pgrep -f utequalizer_start.sh | xargs --no-run-if-empty kill
+pgrep -f move_sink_inputs_to_utequalizer.sh | xargs --no-run-if-empty kill
 #pacmd set-default-sink $(head -1 /home/phablet/.config/utequalizer.bigbrotherisstillwatching/utequalizer.bigbrotherisstillwatching.defaultsink.txt)
 pactl list short sink-inputs | awk '{print $1}' | xargs -I {} pacmd move-sink-input {} $(head -1 /home/phablet/.config/utequalizer.bigbrotherisstillwatching/utequalizer.bigbrotherisstillwatching.defaultsink.txt)
 pacmd unload-module $(head -1 /home/phablet/.config/utequalizer.bigbrotherisstillwatching/utequalizer.bigbrotherisstillwatching.utequalizermodule.txt)
