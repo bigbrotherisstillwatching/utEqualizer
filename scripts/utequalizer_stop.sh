8a5d90307736f5c1bb467aa9011d83f9ebbb0647
bigbrotherisstillwatching <140657673+bigbrotherisstillwatching@users.noreply.github.com>
2026-01-28 10:46:37 +0100

    Update utequalizer_stop.sh

--- scripts/utequalizer_stop.sh
+++ scripts/utequalizer_stop.sh
@@ -3,7 +3,7 @@
 #var1=$(pgrep -f utequalizer_start.sh)
 #var2=$(pgrep -f move_sink_inputs_to_utequalizer.sh)
 
-sleep 0.5
+#sleep 0.5
 
 #if [[ $(pacmd list-sinks | grep -A1 '* index:' | sed 's/.*<//; s/>.*//' | grep -v '* index:') == equalizer ]]
 #then
@@ -16,8 +16,12 @@ sleep 0.5
 #    kill "$var2"
 #fi
 #pgrep -f utequalizer_start.sh | xargs --no-run-if-empty kill
-pgrep -f move_sink_inputs_to_utequalizer.sh | xargs --no-run-if-empty kill
+#pgrep -f move_sink_inputs_to_utequalizer.sh | xargs --no-run-if-empty kill
 #pacmd set-default-sink $(head -1 /home/phablet/.config/utequalizer.bigbrotherisstillwatching/utequalizer.bigbrotherisstillwatching.defaultsink.txt)
+systemctl --user stop utequalizer.service
+systemctl --user disable utequalizer.service
+rm -rf /home/phablet/.config/systemd/user/utequalizer.service
+systemctl --user daemon-reload
 pactl list short sink-inputs | awk '{print $1}' | xargs -I {} pacmd move-sink-input {} $(head -1 /home/phablet/.config/utequalizer.bigbrotherisstillwatching/utequalizer.bigbrotherisstillwatching.defaultsink.txt)
 pacmd unload-module $(head -1 /home/phablet/.config/utequalizer.bigbrotherisstillwatching/utequalizer.bigbrotherisstillwatching.utequalizermodule.txt)
 #else
