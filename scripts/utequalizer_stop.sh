7c47fe2beb350ebdfd618d2a7269a1e89faa5153
bigbrotherisstillwatching <140657673+bigbrotherisstillwatching@users.noreply.github.com>
2026-01-30 16:30:38 +0100

    Update utequalizer_stop.sh

--- scripts/utequalizer_stop.sh
+++ scripts/utequalizer_stop.sh
@@ -3,8 +3,8 @@
 sleep 0.5
 
 systemctl --user stop utequalizer.service
-systemctl --user disable utequalizer.service
-rm -f /home/phablet/.config/systemd/user/utequalizer.service
-systemctl --user daemon-reload
+#systemctl --user disable utequalizer.service
+#rm -f /home/phablet/.config/systemd/user/utequalizer.service
+#systemctl --user daemon-reload
 pactl list short sink-inputs | awk '{print $1}' | xargs -I {} pacmd move-sink-input {} $(head -1 /home/phablet/.config/utequalizer.bigbrotherisstillwatching/utequalizer.bigbrotherisstillwatching.defaultsink.txt)
 pacmd unload-module $(head -1 /home/phablet/.config/utequalizer.bigbrotherisstillwatching/utequalizer.bigbrotherisstillwatching.utequalizermodule.txt)
