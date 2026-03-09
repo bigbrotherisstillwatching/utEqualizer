424c5a4f3e31ab069e2b90df04d0b1d9b7cff920
bigbrotherisstillwatching <140657673+bigbrotherisstillwatching@users.noreply.github.com>
2026-03-09 12:19:49 +0100

    Update utequalizer_service_uninstall.sh

--- scripts/utequalizer_service_uninstall.sh
+++ scripts/utequalizer_service_uninstall.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-#systemctl --user disable utequalizer.service
 systemctl --user stop utequalizer.service
 rm -f /home/phablet/.config/systemd/user/utequalizer.service
 systemctl --user daemon-reload
