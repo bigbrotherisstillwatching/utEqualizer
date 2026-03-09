873406c995c0464e42528b757c5633f4e61994bc
bigbrotherisstillwatching <140657673+bigbrotherisstillwatching@users.noreply.github.com>
2026-03-09 12:19:28 +0100

    Update utequalizer_service_install.sh

--- scripts/utequalizer_service_install.sh
+++ scripts/utequalizer_service_install.sh
@@ -2,4 +2,3 @@
 
 cp -a /opt/click.ubuntu.com/utequalizer.bigbrotherisstillwatching/1.1.0/service/utequalizer.service /home/phablet/.config/systemd/user
 systemctl --user daemon-reload
-#systemctl --user enable utequalizer.service
